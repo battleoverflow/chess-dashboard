9631ec19c3971f9215a0bea27ade1919a230d1fa
azazelm3dj3d <56496067+azazelm3dj3d@users.noreply.github.com>
2023-06-27 20:21:33 -0500

    Better rendering and allows for input (+ started Vercel config)
    - Added local Tailwind styling for better loading
    - Now allows for input in the UI instead of env variables
    - Started Vercel configuration for production release

--- app.py
+++ app.py
@@ -1,25 +1,105 @@
 import datetime
-from flask import Flask, render_template
-from chess import Chess
+from flask import Flask, render_template, request
+from chessdotcom import get_player_profile, get_player_stats
+from chessdotcom.types import ChessDotComError
 
-app   = Flask(__name__)
-chess = Chess()
+app = Flask(__name__)
 
-@app.route("/")
+def get_profile(username):
+    """
+    rtype dict
+    """
+
+    response_profile = get_player_profile(username)
+
+    profile = {
+        "avatar": response_profile.json['player']['avatar'],
+        "player_id": response_profile.json['player']['player_id'],
+        "username": response_profile.json['player']['username'],
+        "title": "N/A",
+        "followers": response_profile.json['player']['followers'],
+        "last_online": response_profile.json['player']['last_online'],
+        "is_streamer": response_profile.json['player']['is_streamer'],
+        "twitch_url": "N/A"
+    }
+
+    if "title" in response_profile.json['player']:
+        # Not everyone will have a title
+        profile['title'] = response_profile.json['player']['title']
+
+    if profile['is_streamer']:
+        # Not everyone will have a twitch
+        profile['twitch_url'] = response_profile.json['player']['twitch_url']
+
+    return profile
+
+def get_stats(username):
+    """
+    rtype dict
+    """
+
+    response_stats = get_player_stats(username)
+
+    stats = {
+        "highest_elo": response_stats.json['stats']['tactics']['highest']['rating'],
+        "lowest_elo": response_stats.json['stats']['tactics']['lowest']['rating']
+    }
+
+    return stats
+
+def calculate_increase(elo_values):
+    try:
+        # (abs(current_elo - previous_elo) / previous_elo) * 100.0
+        percentage = (abs(elo_values[0] - elo_values[1]) / elo_values[1]) * 100.0
+    except ZeroDivisionError:
+        return 0
+
+    return round(percentage, 2)
+
+@app.route("/", methods = ["POST", "GET"])
 def index():
-    chess_template = render_template(
-        "index.html", # Template
-        avatar=chess.get_profile()['avatar'],
-        player_id=chess.get_profile()['player_id'],
-        username=chess.get_profile()['username'],
-        title=chess.get_profile()['title'],
-        followers=chess.get_profile()['followers'],
-        last_online=datetime.datetime.fromtimestamp(chess.get_profile()['last_online']).strftime("%m/%d/%Y @ %H:%M:%S"),
-        is_streamer=chess.get_profile()['is_streamer'],
-        twitch_url=chess.get_profile()['twitch_url'],
-        highest_elo=chess.get_stats()['highest_elo'],
-        lowest_elo=chess.get_stats()['lowest_elo']
-    )
+    username = 0
+
+    if request.method == 'POST':
+        form_data = request.form
+
+        for _, value in form_data.items():
+            username = value
+            break
+    else:
+        form_data = "N/A"
+
+    try:
+        chess_template = render_template(
+            "index.html", # Template
+            form_data=form_data,
+            avatar=get_profile(username)['avatar'],
+            player_id=get_profile(username)['player_id'],
+            username=get_profile(username)['username'],
+            title=get_profile(username)['title'],
+            followers=get_profile(username)['followers'],
+            last_online=datetime.datetime.fromtimestamp(get_profile(username)['last_online']).strftime("%m/%d/%Y @ %H:%M:%S"),
+            is_streamer=get_profile(username)['is_streamer'],
+            twitch_url=get_profile(username)['twitch_url'],
+            highest_elo=get_stats(username)['highest_elo'],
+            lowest_elo=get_stats(username)['lowest_elo'],
+            elo_increase=calculate_increase([get_stats(username)['highest_elo'], get_stats(username)['lowest_elo']])
+        )
+    except ChessDotComError:
+        chess_template = render_template(
+            "index.html", # Template
+            avatar="/static/img/missing_user.png",
+            player_id="N/A",
+            username="N/A",
+            title="N/A",
+            followers="N/A",
+            last_online="N/A",
+            is_streamer="N/A",
+            twitch_url="N/A",
+            highest_elo="N/A",
+            lowest_elo="N/A",
+            elo_increase="N/A"
+        )
 
     return chess_template
 
